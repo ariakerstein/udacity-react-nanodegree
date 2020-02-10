eb2337d037ed3f61cc65294565d3c86aa8327bf6
Ari <ariakerstein@gmail.com>
2020-02-10 14:32:42 -0800

    first commit
    first commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+    showSearchPage: true
   }
 
   render() {
@@ -40,7 +40,7 @@ class BooksApp extends React.Component {
         ) : (
           <div className="list-books">
             <div className="list-books-title">
-              <h1>MyReads</h1>
+              <h1>MyGoodReads</h1>
             </div>
             <div className="list-books-content">
               <div>
@@ -168,6 +168,24 @@ class BooksApp extends React.Component {
                           </div>
                           <div className="book-title">Oh, the Places You'll Go!</div>
                           <div className="book-authors">Seuss</div>
+                        </div>
+                      </li>
+                      <li>
+                        <div className="book">
+                          <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 174, backgroundImage: 'url("http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api")' }}></div>
+                            <div className="book-shelf-changer">
+                              <select>
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title">Chemolog; meditations while battling cancer</div>
+                          <div className="book-authors">A. Samuel Akerstein</div>
                         </div>
                       </li>
                       <li>
